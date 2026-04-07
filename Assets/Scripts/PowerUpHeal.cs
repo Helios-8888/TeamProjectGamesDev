a9bb1b8ca759446e17c7f1f465707185fef8c8e1
Helios-8888 <97148237+Helios-8888@users.noreply.github.com>
2026-04-07 19:35:48 +0100

    AGAIN

--- Assets/Scripts/PowerUpHeal.cs
+++ Assets/Scripts/PowerUpHeal.cs
@@ -6,20 +6,11 @@ public class PowerUpHeal : MonoBehaviour
 
     public GameObject pickupEffect; //Attach some sort of vfx to this
     private PlayerHealthbar MaxHP;
-    //public GameObject pickupHeal; unused variable
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //This doesn't seem right
-            /*
-            if (MaxHP != null)
-            {
-                DontDestroyOnLoad(MaxHP.gameObject);
-            }
-            */
-
             if (other.TryGetComponent<EntityHealth>(out EntityHealth playerHealth))
             {
                 Pickup(other);
