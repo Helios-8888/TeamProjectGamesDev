e334af9e5126431141702a2d07abe1e26fbcd34c
Helios-8888 <97148237+Helios-8888@users.noreply.github.com>
2026-04-14 12:27:28 +0100

    Spawner
    slight script changes

--- Assets/Scripts/Enemy Scripts/Spawner.cs
+++ Assets/Scripts/Enemy Scripts/Spawner.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -23,30 +22,4 @@ public class Spawner : MonoBehaviour
         // Instantiate the selected enemy
         Instantiate(enemies[randomIndex], transform.position, transform.rotation);
     }
-=======
-using UnityEngine;
-
-public class Spawner : MonoBehaviour
-{
-    public GameObject Melee;
-    public GameObject Tank;
-    public GameObject Ranged;
-
-    void Start()
-    {
-        SpawnEnemy();
-    }
-
-    private void SpawnEnemy()
-    {
-        // Create an array of enemy prefabs
-        GameObject[] enemies = { Melee, Tank, Ranged };
-
-        // Choose a random index
-        int randomIndex = Random.Range(0, enemies.Length);
-
-        // Instantiate the selected enemy
-        Instantiate(enemies[randomIndex], transform.position, transform.rotation);
-    }
->>>>>>> Stashed changes
 }
